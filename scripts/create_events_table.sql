772c97e8e525eb73ad80d6cdf175903dfabafedb
Martin Komischke <martin.komischke@outlook.com>
2020-10-27 16:36:27 +0100

    move logic from the create teams into the create events table scripts

--- scripts/create_events_table.sql
+++ scripts/create_events_table.sql
@@ -1,3 +1,10 @@
+-- TEAMS
+drop table if exists teams;
+select distinct(team) as description into teams
+from import.master_plan;
+alter table teams
+add id serial primary key;
+-- EVENTS
 drop table if exists events;
 create table events(
     id serial primary key,
--- scripts/create_teams_table.sql
+++
@@ -1,5 +0,0 @@
-drop table if exists teams;
-select distinct(team) as description into teams
-from import.master_plan;
-alter table teams
-add id serial primary key;
