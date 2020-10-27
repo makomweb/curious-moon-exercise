192bb73f0723fbe2e91a5092d8f17990988c7b79
Martin Komischke <martin.komischke@outlook.com>
2020-10-27 16:39:23 +0100

    add the other lookup tables

--- scripts/normalize.sql
+++ scripts/normalize.sql
@@ -4,6 +4,30 @@ select distinct(team) as description into teams
 from import.master_plan;
 alter table teams
 add id serial primary key;
+-- SPASS TYPES
+drop table if exists spass_types;
+select distinct(spass_type) as description into spass_types
+from import.master_plan;
+alter table spass_types
+add id serial primary key;
+-- TARGET
+drop table if exists targets;
+select distinct(target) as description into targets
+from import.master_plan;
+alter table targets
+add id serial primary key;
+-- EVENT TYPES
+drop table if exists event_types;
+select distinct(library_definition) as description into event_types
+from import.master_plan;
+alter table event_types
+add id serial primary key;
+--REQUESTS
+drop table if exists requests;
+select distinct(request_name) as description into requests
+from import.master_plan;
+alter table requests
+add id serial primary key;
 -- EVENTS
 drop table if exists events;
 create table events(
