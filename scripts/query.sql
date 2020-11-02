8de786ea993aa0ace7aff82498ab2756b537adbe
Martin Komischke <martin.komischke@outlook.com>
2020-11-02 15:32:04 +0100

    query the thermal events

--- scripts/query.sql
+++ scripts/query.sql
@@ -1,3 +1,5 @@
-select *
-from targets
-where description = 'Enceladus';
+-- find the thermal results
+select id, date, title
+from enceladus_events
+where date between '2005-02-01'::date and '2005-02-28'::date
+and search @@ to_tsquery('thermal');
