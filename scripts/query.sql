561d2d6e75cdfef3a19e3ecc256b6611ef5c9a83
Martin Komischke <martin.komischke@outlook.com>
2020-11-02 15:36:05 +0100

    query the events table

--- scripts/query.sql
+++ scripts/query.sql
@@ -1,8 +1,2 @@
-select count(1) as activity,
-    teams.description
-from events
-    inner join teams on teams.id = team_id
-where time_stamp::date = '2005-03-09'
-    and target_id = 28
-group by teams.description
-order by activity desc;
+select id, title from enceladus_events
+where search @@ to_tsquery('closest');
