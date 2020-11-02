7ecee8ec90c610a5831a18335de50e2ef59a21bb
Martin Komischke <martin.komischke@outlook.com>
2020-11-02 15:47:52 +0100

    fire up another query

--- scripts/query.sql
+++ scripts/query.sql
@@ -1,2 +1,5 @@
-select id, title from enceladus_events
-where search @@ to_tsquery('closest');
+select time_stamp,
+    title
+from events
+where time_stamp::date = '2009-11-02'
+order by time_stamp;
