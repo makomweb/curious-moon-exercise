4122d2a7639f4660aee9c0f90196b14e67107dfa
Martin Komischke <martin.komischke@outlook.com>
2020-11-09 17:25:50 +0100

    fix whitespaces

--- scripts/create_table_cda.sql
+++ scripts/create_table_cda.sql
@@ -1,18 +1,18 @@
 drop table if exists import.cda;
 create table import.cda(
-  event_id text,
-  impact_event_time text,
-  impact_event_julian_date text,
-  qp_amplitude text,
-  qi_amplitude text,
-  qt_amplitude text,
-  qc_amplitude text,
-  spacecraft_sun_distance text,
-  spacecraft_saturn_distance text,
-  spacecraft_x_velocity text,
-  spacecraft_y_velocity text,
-  spacecraft_z_velocity text,
-  counter_number text,
-  particle_mass text,
-  particle_charge text
+    event_id text,
+    impact_event_time text,
+    impact_event_julian_date text,
+    qp_amplitude text,
+    qi_amplitude text,
+    qt_amplitude text,
+    qc_amplitude text,
+    spacecraft_sun_distance text,
+    spacecraft_saturn_distance text,
+    spacecraft_x_velocity text,
+    spacecraft_y_velocity text,
+    spacecraft_z_velocity text,
+    counter_number text,
+    particle_mass text,
+    particle_charge text
 );
