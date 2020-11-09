8c6e2e8e3c64fcc37920dc6fb3a5bd109585f5f5
Martin Komischke <martin.komischke@outlook.com>
2020-11-09 13:49:28 +0100

    add year and week to the flyby-altitudes-view

--- scripts/create_flyby_altitudes_view.sql
+++ scripts/create_flyby_altitudes_view.sql
@@ -1,6 +1,8 @@
 drop materialized view if exists flyby_altitudes;
 create materialized view flyby_altitudes as
 select (sclk::timestamp) as time_stamp,
+    date_part('year', (sclk::timestamp)) as year,
+    date_part('week', (sclk::timestamp)) as week,
     alt_t::numeric(10, 3) as altitude
 from import.inms
 where target = 'ENCELADUS'
