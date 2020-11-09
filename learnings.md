eb8c2f207c6bedc3ef5dc93f44b8e019cf1b113a
Martin Komischke <martin.komischke@outlook.com>
2020-11-09 14:09:41 +0100

    simplify cte for selecting the timestamps of each nadir per flyby

--- learnings.md
+++ learnings.md
@@ -167,3 +167,23 @@ nadirs as (
 select nadir, altitude
 from nadirs;
 ~~~
+
+And with the stored function it can be simplified:
+
+~~~
+with lows_by_week as (
+    select year,
+        week,
+        min(altitude) as altitude
+    from flyby_altitudes
+    group by year,
+        week
+),
+nadirs as (
+    select low_time(altitude, year, week) as time_stamp,
+        altitude
+    from lows_by_week
+)
+select *
+from nadirs;
+~~~
--- scripts/cte_fly_by_altitudes.sql
+++ scripts/cte_fly_by_altitudes.sql
@@ -1,16 +1,15 @@
 with lows_by_week as (
-    select year, week, min(altitude) as altitude
+    select year,
+        week,
+        min(altitude) as altitude
     from flyby_altitudes
-    group by year, week
+    group by year,
+        week
 ),
 nadirs as (
-    select (min(time_stamp) + (max(time_stamp) - min(time_stamp)) / 2) as nadir, lows_by_week.altitude
-    from flyby_altitudes, lows_by_week
-    where flyby_altitudes.altitude = lows_by_week.altitude
-        and flyby_altitudes.year = lows_by_week.year
-        and flyby_altitudes.week = lows_by_week.week
-    group by lows_by_week.altitude
-    order by nadir
+    select low_time(altitude, year, week) as time_stamp,
+        altitude
+    from lows_by_week
 )
-select nadir, altitude
+select *
 from nadirs;
