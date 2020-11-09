0244887c1bd737fc0c5dac05fe7471c8c7beb02f
Martin Komischke <martin.komischke@outlook.com>
2020-11-09 17:26:10 +0100

    start normalizing CDA data

---
+++ scripts/normalize_cda.sql
@@ -0,0 +1,15 @@
+drop schema if exists cda cascade;
+create schema cda;
+select event_id::integer as id,
+    impact_event_time::timestamp as time_stamp,
+    impact_event_time::date as impact_date,
+    counter_number::integer,
+    spacecraft_sun_distance::numeric(6, 4) as sun_distance_au,
+    spacecraft_saturn_distance::numeric(8, 2) as saturn_distance_rads,
+    spacecraft_x_velocity::numeric(6, 2) as x_velocity,
+    spacecraft_y_velocity::numeric(6, 2) as y_velocity,
+    spacecraft_z_velocity::numeric(6, 2) as z_velocity,
+    particle_charge::numeric(4, 1),
+    particle_mass::numeric(4, 1)
+from import.cda
+order by impact_event_time::timestamp;
